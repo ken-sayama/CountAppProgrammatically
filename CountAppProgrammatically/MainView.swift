886f851d23a43c604b0c129a8a7660a5fd70db34
Ken Sayama <sayamaken0402@gmail.com>
2018-05-19 09:20:16 +0900

    create MainView.swift

---
+++ CountAppProgrammatically/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  CountAppProgrammatically
+//
+//  Created by Ken Sayama on 2018/05/19.
+//  Copyright © 2018年 Ken Sayama. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}

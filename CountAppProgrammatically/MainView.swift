237b13a64c667bbe4a88a388e24b7ef69094568e
Ken Sayama <sayamaken0402@gmail.com>
2018-05-21 01:01:03 +0900

    add label and buttons

--- CountAppProgrammatically/MainView.swift
+++ CountAppProgrammatically/MainView.swift
@@ -10,12 +10,46 @@ import UIKit
 
 class MainView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let numberLabel = UILabel()
+    let plusBtn = UIButton()
+    let minusBtn = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(numberLabel)
+        addSubview(plusBtn)
+        addSubview(minusBtn)
     }
-    */
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        let labelSize = self.numberLabel.sizeThatFits(self.bounds.size)
+        let x = (self.bounds.width - labelSize.width) / 2
+        let y = (self.bounds.height - labelSize.height) / 3
+        let labelOrigin = CGPoint(x: x, y: y)
+        
+        numberLabel.frame = CGRect(origin: labelOrigin, size: labelSize)
+        numberLabel.textColor = .black
+        numberLabel.text = "0"
+        numberLabel.font = UIFont.systemFont(ofSize: 30)
+        numberLabel.textAlignment = .center
+        
+        plusBtn.frame = CGRect(x: bounds.width - 170, y: numberLabel.frame.maxY + 50, width: 150, height: 150)
+        plusBtn.setTitle("Plus", for: .normal)
+        plusBtn.layer.cornerRadius = 75.0
+        plusBtn.backgroundColor = .blue
+        
+        minusBtn.frame = CGRect(x: bounds.origin.x + 20, y: numberLabel.frame.maxY + 50, width: 150, height: 150)
+        minusBtn.setTitle("Minus", for: .normal)
+        minusBtn.layer.cornerRadius = 75.0
+        minusBtn.backgroundColor = .red
+    }
+    
 }
--- CountAppProgrammatically/ViewController.swift
+++ CountAppProgrammatically/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //let mainView = MainView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         navigationItem.title = "CountAppProgramatically"
         
+        // BaseView.swiftを作成
+        //let baseView = BaseView(frame: self.view.bounds)
+        let mainView = MainView(frame: self.view.bounds)
+        
+        // 自動でリサイズしてくれる設定
+        mainView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        self.view.addSubview(mainView)
     }
 
     override func didReceiveMemoryWarning() {
